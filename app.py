02c07458874bf06c2b07c0fde098110851b62435
davidoffglds <165810982+davidoffglds@users.noreply.github.com>
2024-04-09 13:26:05 +0000

    added list job to html

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
-from flask import Flask
+from flask import Flask,render_template,jsonify
 
 app = Flask(__name__)
 
+
+
+JOBS = [
+    {
+        "id": 1,
+        "title": "Pompier",
+        "location": "Romania",
+        "salary": "120,000 RON"
+    },
+    {
+        "id": 2,
+        "title": "Salvamar",
+        "location": "Romania, Bulgaria",
+        "salary": "130,000 RON"
+    },
+    {
+        "id": 3,
+        "title": "Politist",
+        "location": "Romania",
+        "salary": "180,000 RON"
+    },
+    {
+        "id": 4,
+        "title": "Gradinar",
+        "location": "Romania",
+        "salary": "120,000 RON"  
+    }
+]
+
+        
+
 @app.route("/")
 def hey():
-    return "laba"
+    return render_template("home.html", jobs=JOBS)
+
+@app.route("/api")
+def list_job():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
